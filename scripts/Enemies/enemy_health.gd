9108aa5c695f17870b9f7dbd2c9341eae15ad11b
Anthony <markanthony04@gmail.com>
2026-05-31 00:02:12 +0800

    feat: spawn points fixed

--- scripts/Enemies/enemy_health.gd
+++ scripts/Enemies/enemy_health.gd
@@ -1,5 +1,7 @@
-extends RigidBody2D
+extends CharacterBody2D
 
+const SPEED = 60.0
+var forge_position = Vector2(544, 288)
 
 @export var max_health: int = 30
 var current_health: int
@@ -7,7 +9,12 @@ var current_health: int
 func _ready():
 	current_health = max_health
 
+func _physics_process(_delta):
+	var direction = (forge_position - global_position).normalized()
+	velocity = direction * SPEED
+	move_and_slide()
+
 func take_damage(amount: int):
 	current_health -= amount
 	if current_health <= 0:
-		get_parent().die()
+		queue_free()
