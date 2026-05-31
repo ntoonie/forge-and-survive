5aedee03609842ad4e854a7449e7e9e85c20b010
Anthony <markanthony04@gmail.com>
2026-06-01 00:40:23 +0800

    feat: added audio for victory and defeat screens

--- scripts/ui/victory_screen.gd
+++ scripts/ui/victory_screen.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	$Button.connect("pressed", _on_button_pressed)
+	AudioManager.play_victory()
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
